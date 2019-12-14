0115dbeeed790eb0c52ee1ecfe571bc31dbd6f6d
Robert Biggs <rbiggs@ymail.com>
2019-12-14 09:56:08 -0800

    Updated how types are handled.
    1. Updated version of TypeScript.
    2. Autogenerated d.ts file from JSDoc comments.
    3. Added src/types.js where all project-wide types are amalgamented.

--- src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { run } from './runtime'
-export { union } from './union'
-export { batchEffects, batch } from './effects'
-
-// Export types for @composi/runtime:
-export { Send, Message, State, GetState, Program } from './runtime'
-
---
+++ src/types.js
@@ -0,0 +1,32 @@
+// Dummy export to make this a module with exportable types.
+export {}
+
+/**
+ * @typedef {Object} Tag
+ * @prop {string} type
+ * @prop {any} [data]
+ */
+
+/**
+ * @typedef {Object<string, any>} Message
+ * @prop {string} type
+ * @prop {any} [data]
+ * @typedef {(msg?: Message | Function, data?: any) => Message} Send
+ * @typedef {() => State} GetState
+ */
+/**
+ * @typedef {any} State Simple or complex types for application state.
+ */
+/**
+ * @typedef {State | void} InitResult Return result of program init method.
+ */
+/**
+ * @typedef {Object<string, any>} Program A program to run.
+ * @prop {() => InitResult} init Method to set up initial state.
+ * @prop {(state: State, send?: Send) => void} view Method to present the current application state.
+ * @prop {(state: State, msg?: Message, send?: Send) => any} update Method to capture messages sent from view or subscriptions. According to the message, an action will transform application state and pass it the the program view method.
+ * @prop {(send?: Send, getState?: GetState) => void} [subscriptions] Method to run effects when the program starts. These run independently from the rest of the program.
+ * @prop {(send?: Send, getState?: GetState) => void} [subs] Shortcut for subscriptions.
+ * @prop {(state: State) => void} [done] Method to do clean up when shutting down a program.
+ * @prop {Send} [send] A static send function for dispatching message to a program. Used with routers and in composition.
+ */
